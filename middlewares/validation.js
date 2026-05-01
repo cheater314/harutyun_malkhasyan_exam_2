ede2b0d5998d8538c26ff7cf4f45fe0390208f53
cheater314 <iphoneakk314@gmail.com>
2026-05-01 20:15:12 +0400

    add schemas and auth schema

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,3 +1,25 @@
 import createError from "http-errors";
 import _ from "lodash";
 
+export default function validator(schema, path = "body") {
+    return (req, res, next) => {
+        try {
+            const { error } = schema.validate(req[path], {
+                abortEarly: false
+            });
+
+            if (!error) return next();
+
+            const errors = {};
+
+            error.details.forEach(err => {
+                const key = err.path.join(".");
+                errors[key] = err.message.replace(/\"/g, "");
+            });
+
+            throw createError(422, "Validation error", { errors });
+        } catch (e) {
+            next(e);
+        }
+    };
+}
